9236ce98619d8334ee8f7ca8c779ef0176b6572e
dannyrgd <danny.ramirezgd@gmail.com>
2021-12-19 14:38:31 -0500

    fetching data based on username passed as argument

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,26 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function(user) {
+    //forma the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-getUserRepos();
+    //make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+};
+//     fetch("https://api.github.com/users/octocat/repos")
+//     .then(function(response) {
+//         response.json().then(function(data){
+//             console.log(data);
+//         });
+//         console.log("inside", response)
+//     });
+//     console.log("outside");
+// };
+
+var response = fetch("https://api.github.com/users/octocat/repos");
+console.log(response);
+getUserRepos("microsoft");
+getUserRepos("facebook");
+getUserRepos("dannyramirezgd");
